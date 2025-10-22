47bb731ff3aa55c1dda7cc3b74421e3d3d7332cb
Erick Yoshike <ericktakao28@gmail.com>
2025-10-22 20:05:13 -0300

    fix: updating api config

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,12 +4,14 @@ import type {
   TumuloResponse,
 } from '@/interfaces/Tumulo';
 
-const API_URL = process.env.REACT_APP_API_URL
-  ? `${process.env.REACT_APP_API_URL}/tumulos`
-  : 'http://localhost:3333/tumulos';
+const API_URL = import.meta.env.REACT_APP_API_URL
+  ? `${import.meta.env.REACT_APP_API_URL}/tumulos`
+  : 'http://localhost:3000/tumulos';
 
 export async function getAllTumulos(): Promise<Tumulo[]> {
   const res = await fetch(`${API_URL}/all`);
+  console.log(res);
+
   if (!res.ok) {
     throw new Error('Erro ao buscar túmulos');
   }
